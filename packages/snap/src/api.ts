271638ae4823cde1c6f53c7d98dc90281a18ffe3
Facundo La Rocca <facu.larocca@gmail.com>
2024-06-12 08:06:27 -0300

    Public APIKEY added

--- packages/snap/src/api.ts
+++ packages/snap/src/api.ts
@@ -1,3 +1,5 @@
+const PYXINE_PUBLIC_METAMASK_API_KEY =
+  'apikey_m5bo53KvJOlxTv08UtxFILi6gwwQbuJB';
 const PYXINE_API_BASE_URL = 'https://api.pyxine.io/v1/score';
 
 export type Score = {
@@ -12,12 +14,17 @@ export type Score = {
 export async function getScore(transaction: { to: string }): Promise<Score> {
   let scoreResult = await fetch(`${PYXINE_API_BASE_URL}/${transaction.to}`, {
     headers: {
+      'x-api-key': PYXINE_PUBLIC_METAMASK_API_KEY,
       'x-cached': 'true',
     },
   });
 
   if (scoreResult.status === 404) {
-    scoreResult = await fetch(`${PYXINE_API_BASE_URL}/${transaction.to}`);
+    scoreResult = await fetch(`${PYXINE_API_BASE_URL}/${transaction.to}`, {
+      headers: {
+        'x-api-key': PYXINE_PUBLIC_METAMASK_API_KEY,
+      },
+    });
   }
 
   if (scoreResult.status !== 200) {
